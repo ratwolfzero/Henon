245783394c7ab91259cde9ba94e075f00b409e76
Ralf <ratwolf@duck.com>
2025-02-05 01:29:05 +0100

    modified:   henon.jl

--- henon.jl
+++ henon.jl
@@ -1,19 +1,18 @@
 using PyPlot
-pygui(true)  # Open plot in a separate interactive window
+pygui(true)
 
 function henon(num, a, b)  
-    x, y = 0.0, 0.0  # Origin point
-    u = Vector{Float64}(undef, num)  # Preallocate x-coordinates
-    v = Vector{Float64}(undef, num)  # Preallocate y-coordinates
+    x, y = 0.0, 0.0
+    u = Vector{Float64}(undef, num)
+    v = Vector{Float64}(undef, num)
 
     for i in 1:num
         u[i], v[i] = x, y
-        x, y = 1 - a * x^2 + y, b * x  # Tuple unpacking for efficiency
+        x, y = 1 - a * x^2 + y, b * x
     end
 
-    figure = PyPlot.gcf()
-    figure.set_size_inches(12, 8)
-    scatter(u, v, s=0.0001, marker=".", c="red")  # Plot points
+    fig, ax = subplots(figsize=(12, 8))  # Explicit figure creation
+    scatter(u, v, s=0.0001, marker=".", c="red")
 end
 
 henon(1_000_000, 1.4, 0.3)
